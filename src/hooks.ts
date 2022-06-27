d5e2dcf020d29e1cabb3b98ce1ef25f23d42bd32
Anwesh43 <anweshthecool0@gmail.com>
2022-06-27 16:11:03 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const delay : number = 20 
 const scGap : number = 0.02 
@@ -35,10 +35,48 @@ export const useDimension = () => {
             setH(window.innerHeight)
         }
         return () => {
-            
+
         }
     }, [])
     return {
         w, h 
     }
 }
+
+const sinify : Function = (scale : number) => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const size : number = Math.min(w, h) / 10 
+    const x : number = w / 2 
+    const y : number = h / 2
+    return {
+        parentStyle() : CSSProperties {
+           return {
+                position: 'absolute', 
+                left: `${w / 2}px`,
+                top: `${h / 2}px`
+           }
+        },
+        boxStyle() : CSSProperties {
+            return {
+                position: 'absolute',
+                width: `${size}px`,
+                height: `${size}px`,
+                top: `${-size / 2}px`,
+                left: `${-size / 2}px`,
+                background: 'indigo',
+                opacity: 1 - sinify(scale)
+            }
+        },
+        textStyle() : CSSProperties {
+            return {
+                fontSize : `${Math.min(w, h) / 40}px`,
+                left: `${-(w / 2 - 100) + (w / 2) * sinify(scale)}px`,
+                top: `-100px`,
+                width: '200px',
+                height: '200px',
+                color: 'black'
+            }
+        }
+    }
+} 
